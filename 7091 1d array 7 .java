241f2fd6cc35c1897f2c46d791bbb569cf1f4388
Gurjeetbains07 <144418591+Gurjeetbains07@users.noreply.github.com>
2023-11-16 19:41:17 +0530

    Update and rename 7091 array.java to 7091 1d array 7 .java

--- 7091 1d array 7 .java
+++ 7091 1d array 7 .java
@@ -2,13 +2,9 @@ import java.util.Scanner;
 
 class Evenposition {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input the size of the array
+        Scanner scanner = new Scanner(System.in)
         System.out.print("Enter the size of the array: ");
         int size = scanner.nextInt();
-
-        // Input array elements
         int[] array = new int[size];
         System.out.println("Enter the elements of the array:");
 
@@ -16,8 +12,6 @@ class Evenposition {
             System.out.print("Enter element at position " + (i + 1) + ": ");
             array[i] = scanner.nextInt();
         }
-
-        // Print elements at even positions
         System.out.println("\nElements at even positions:");
 
         for (int i = 1; i < size; i += 2) {
